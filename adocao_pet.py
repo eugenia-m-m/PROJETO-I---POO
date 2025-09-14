e447084141f9061e4384a53d4ff21a62a0d3f114
eugenia-m-m <mariaeugeniamagalhaesmarques@gmail.com>
2025-09-14 12:51:54 -0300

    Update adocao_pet.py

--- adocao_pet.py
+++ adocao_pet.py
@@ -8,13 +8,18 @@
 
 
 class Animais:
-    def __init__(self, nome, especie, sexo, idade, raca):
+    def __init__(self, id, nome, especie, sexo, idade, raca, disponibilidade):
+        self.__id = id
         self.nome = nome
         self.__especie = especie
         self.__sexo = sexo
         self.__idade = idade
         self.__raca = raca
+        self.__disponibilidade = disponibilidade
     
+    @property
+    def mostrar_id (self):
+        return self.__id
     @property
     def mostrar_especie (self):
         return self.__especie
@@ -30,6 +35,27 @@ class Animais:
     @property
     def mostrar_raca (self):
         return self.__raca
+    
+    @property
+    def mostrar_disponibilidade (self):
+        return self.__disponibilidade
+    
+    def cadastrar_Animal (self, animal): # Função para cadastrarAnimal
+        if animal not in list_animais:
+            for animal in list_animais:
+                id = input("Digite o id do animal: ")
+                nome = input("Digite o nome do animal: ")
+                especie = input("Digite a especie do animal: ")
+                sexo = input("Digite o sexo do animal: ")
+                idade = input("Digite a idade do animal: ")
+                raca = input("Digite a raca do animal: ")
+                disponibilidade = input("Digite a disponibilidade do animal: ")
+                animal = Animais (id, nome, especie, sexo, idade, raca, disponibilidade)
+            
+            list_animais.append(animal)
+            return True
+        else:
+            return "Animal já cadastrado."
 
     
 class Adotante:
