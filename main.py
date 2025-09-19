f3e4b00c0fa5ec4e5b556a909449679864dcfc1d
Guizinhounder07exc <guimenezes2507@gmail.com>
2025-09-19 08:55:53 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from sistema_adocao import SistemaAdocao
+from animal import Animal 
+
+def menu():
+    while True:
+        print(""" 
+        MENU
+            
+            1- CADASTRAR
+            2- LISTAR
+            3- SAIR
+    """)
+        op = input()
+        if op == '1':
+            print('Cadastrar')
+            break 
+
+        elif op == '2':
+            print('Listar')
+            break 
+
+        elif op == '3':
+            print('Sair')
+            break
+
+        else:
+            print('Opção inválida. Tente novamente!')
+
+
+
+if __name__ == "__main__":
+    menu()
