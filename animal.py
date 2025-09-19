5fbd04d9578cea5d185b5469ecc2a0cdde86e040
Guizinhounder07exc <guimenezes2507@gmail.com>
2025-09-19 09:26:40 -0300

    Update animal.py

---
+++ animal.py
@@ -0,0 +1,9 @@
+class Animal:
+    def __init__(self, id, nome, especie, sexo, idade, raca, disponibilidade):
+        self.id = id
+        self.nome = nome
+        self.especie = especie
+        self.sexo = sexo
+        self.idade = idade
+        self.raca = raca
+        self.disponibilidade = disponibilidade
