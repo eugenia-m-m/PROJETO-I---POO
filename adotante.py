3b6462c0571955c9d8e2859619a0b814b8cc8dc8
eugenia-m-m <mariaeugeniamagalhaesmarques@gmail.com>
2025-09-22 09:05:04 -0300

    Update adotante.py

--- adotante.py
+++ adotante.py
@@ -1,11 +1,14 @@
 class Adotante:
+    cadastro_adotantes = []
+
     def __init__(self, nome, idade, rg, cpf, comprovante):
         self.__nome = nome
         self.__idade = idade
         self.__rg = rg
         self.__cpf = cpf
         self.__comprovante = comprovante
-
+        Adotante.cadastro_adotantes.append(self)
+    
     @property
     def mostrar_nome (self):
         return self.__nome
@@ -15,10 +18,19 @@ class Adotante:
         return self.__idade
     
     @classmethod
-    def cadastrar_adotante (cls, novo_id):
-        if novo_id in Adotante.cadastro_adotante:
-            print('Este ID já está sendo utilizado. Tente outro.')
-            return
+    def listar_adotantes(cls):
+        return cls.cadastro_adotantes  
+    
+    @classmethod
+    def cadastrar_Animal(cls, id, nome, idade, rg, cpf, comprovante, endereco):
+        for canditato in cls.cadastro_adotantes:
+            if canditato.id == id:
+                return "Adotante já cadastrado."
+
+        animal = Adotante(id, nome, idade, rg, cpf, comprovante, endereco)
+        cls.cadastro_adotantes.append(animal)
+        return "Adotante cadastrado com sucesso."
+
     
     @classmethod
     def editar_adotante (cls, novo_id):
